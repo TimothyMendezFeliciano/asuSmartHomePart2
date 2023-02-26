104f89227fc75350d37728c008ff11506341e4a6
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2023-02-25 23:28:33 -0400

    Cant use match

--- utils.py
+++ utils.py
@@ -12,79 +12,79 @@ def last_file_in_folder(directory):
 
 
 def find_train_data_equivalent_key(test_vector_key: str) -> str:
-    match test_vector_key.split('.')[0]:
-        case "H-0":
-            return "Num0"
-        case "H-1":
-            return "Num1"
-        case "H-2":
-            return "Num2"
-        case "H-3":
-            return "Num3"
-        case "H-4":
-            return "Num4"
-        case "H-5":
-            return "Num5"
-        case "H-6":
-            return "Num6"
-        case "H-7":
-            return "Num7"
-        case "H-8":
-            return "Num8"
-        case "H-9":
-            return "Num9"
-        case "H-DecreaseFanSpeed":
-            return "FanDown"
-        case "H-FanOff":
-            return "FanOff"
-        case "H-FanOn":
-            return "FanOn"
-        case "H-IncreaseFanSpeed":
-            return "FanUp"
-        case "H-LightOff":
-            return "LightOff"
-        case "H-LightOn":
-            return "LightOn"
-        case "H-SetThermo":
-            return "SetThermo"
+    key = test_vector_key.split('.')[0]
+    if key == "H-0":
+        return "Num0"
+    if key == "H-1":
+        return "Num1"
+    if key == "H-2":
+        return "Num2"
+    if key == "H-3":
+        return "Num3"
+    if key == "H-4":
+        return "Num4"
+    if key == "H-5":
+        return "Num5"
+    if key == "H-6":
+        return "Num6"
+    if key == "H-7":
+        return "Num7"
+    if key == "H-8":
+        return "Num8"
+    if key == "H-9":
+        return "Num9"
+    if key == "H-DecreaseFanSpeed":
+        return "FanDown"
+    if key == "H-FanOff":
+        return "FanOff"
+    if key == "H-FanOn":
+        return "FanOn"
+    if key == "H-IncreaseFanSpeed":
+        return "FanUp"
+    if key == "H-LightOff":
+        return "LightOff"
+    if key == "H-LightOn":
+        return "LightOn"
+    if key == "H-SetThermo":
+        return "SetThermo"
 
 
 def return_correct_label(label: str) -> str:
-    match label.split('-')[0]:
-        case "Num0":
-            return "0"
-        case "Num1":
-            return "1"
-        case "Num2":
-            return "2"
-        case "Num3":
-            return "3"
-        case "Num4":
-            return "4"
-        case "Num5":
-            return "5"
-        case "Num6":
-            return "6"
-        case "Num7":
-            return "7"
-        case "Num8":
-            return "8"
-        case "Num9":
-            return "9"
-        case "FanDown":
-            return "10"
-        case "FanOff":
-            return "12"
-        case "FanOn":
-            return "11"
-        case "FanUp":
-            return "13"
-        case "LightOff":
-            return "14"
-        case "LightOn":
-            return "15"
-        case "SetThermo":
-            return "16"
+    key = label.split('-')[0]
+    if key == "Num0":
+        return "0"
+    if key == "Num1":
+        return "1"
+    if key == "Num2":
+        return "2"
+    if key == "Num3":
+        return "3"
+    if key == "Num4":
+        return "4"
+    if key == "Num5":
+        return "5"
+    if key == "Num6":
+        return "6"
+    if key == "Num7":
+        return "7"
+    if key == "Num8":
+        return "8"
+    if key == "Num9":
+        return "9"
+    if key == "FanDown":
+        return "10"
+    if key == "FanOff":
+        return "12"
+    if key == "FanOn":
+        return "11"
+    if key == "FanUp":
+        return "13"
+    if key == "LightOff":
+        return "14"
+    if key == "LightOn":
+        return "15"
+    if key == "SetThermo":
+        return "16"
 
 
 def find_comparable_vectors(lookup_key, dictionary) -> []:
